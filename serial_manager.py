d6ee9b98a0f63e02351bc1c32433996490fd14d6
Samuel Hacker <samuel.hacker.mail@gmail.com>
2025-05-01 12:54:54 +0200

    New version - buttons, serial close and open ,etc.

--- serial_manager.py
+++ serial_manager.py
@@ -14,8 +14,13 @@ def init_serial():
     global ser
     try:
         ser = serial.Serial(PORT, BAUDRATE, timeout=1)
-        time.sleep(2)
-        print("Sériové spojenie nadviazané.")
+        time.sleep(2.0)
+        ser.reset_input_buffer()
+        ser.reset_output_buffer()
+        for _ in range(3):
+            ser.write(b'P\n')
+            time.sleep(0.2)
+        print("Príkaz 'P' odoslaný Arduinu po OPEN.")
         return True
     except serial.SerialException as e:
         print("Chyba pri otváraní portu:", e)
@@ -36,6 +41,26 @@ def start_monitoring():
         ser.write(b'A')
         print("Spustené monitorovanie...")
 
+def stop_monitoring():
+    global is_running
+    is_running = False
+    if ser:
+        ser.write(b'S')
+        print("Monitorovanie zastavené a príkaz 'S' odoslaný Arduinu.")
+
+def close_serial():
+    global is_running, ser
+    is_running = False
+    if ser and ser.is_open:
+        try:
+            ser.write(b'X')
+            time.sleep(0.2)
+            print("Príkaz X odoslaný Arduinu.")
+        except Exception as e:
+            print("Chyba pri odosielaní príkazu X:", e)
+        return True
+    return False
+
 def read_serial_data():
     global is_running
     while is_running:
@@ -45,11 +70,14 @@ def read_serial_data():
                 if line:
                     print("Arduino:", line)
                     latest_lines.append(line)
-                    if len(latest_lines) > 10:
+                    if len(latest_lines) > 1200:
                         latest_lines.pop(0)
             except Exception as e:
                 print("Chyba pri čítaní:", e)
         time.sleep(0.1)
 
 def get_latest_data():
-    return latest_lines.copy()
+    return {
+        "data": latest_lines[-15:],  # zoznam posledných výpisov
+        "running": is_running         # stav monitorovania
+    }
